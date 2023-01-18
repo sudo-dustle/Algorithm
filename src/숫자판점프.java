1b11f462c1d190bd72ca989be3bb75dcb8d61426
sudo-dustle <asdf748596123@gmail.com>
2023-01-19 01:43:53 +0900

    fix: import 제거

--- src/숫자판점프.java
+++ src/숫자판점프.java
@@ -1,7 +1,6 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.Set;
 import java.util.StringTokenizer;
