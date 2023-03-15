177185784a357db523fe7380bebca47c690a443c
sudo-dustle <asdf748596123@gmail.com>
2023-03-16 02:16:58 +0900

    memoi 사용

--- src/CountNumberofTeams.java
+++ src/CountNumberofTeams.java
@@ -1,31 +1,43 @@
 public class CountNumberofTeams {
     public static void main(String[] args) {
-        int[] rating = {1, 2, 3, 4};
+        int[] rating = {2,5,3,4,1};
         int answer = numTeams(rating);
         System.out.println(answer);
     }
 
     public static int numTeams(int[] rating) {
+        int[][] memoi = new int[1000][4];
+        int[][] memoi2 = new int[1000][4];
         int count = 0;
-        int length = rating.length;
 
-        for (int i = 0; i < length - 2; i++) {
-            for (int j = i + 1; j < length - 1; j++) {
-                if (rating[i] < rating[j]) {
-                    for (int k = j + 1; k < length; k++) {
-                        if (rating[j] < rating[k]) {
-                            count += 1;
-                        }
-                    }
-                }
-                if (rating[i] > rating[j]) {
-                    for (int k = j + 1; k < length; k++) {
-                        if (rating[j] > rating[k]) {
-                            count += 1;
-                        }
+        for (int i = 0; i < 1000; i++) {
+            memoi[i][1] = 1;
+            memoi2[i][1] = 1;
+        }
+
+        for (int size = 2; size < 4; size++) {
+            for (int i = 0; i < rating.length; i++) {
+                for (int j = 0; j < i; j++) {
+                    if (rating[i] < rating[j]) {
+                        memoi[i][size] += memoi[j][size - 1];
                     }
                 }
             }
+        }
+
+        for (int size = 2; size < 4; size++) {
+            for (int i = 0; i < rating.length; i++) {
+                for (int j = 0; j < i; j++) {
+                    if (rating[i] > rating[j]) {
+                        memoi2[i][size] += memoi2[j][size - 1];
+                    }
+                }
+            }
+        }
+
+        for(int i = 0; i < 1000; i++) {
+            count += memoi[i][3];
+            count += memoi2[i][3];
         }
 
         return count;
